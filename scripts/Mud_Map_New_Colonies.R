6bec4aeb9f14fbc28f54b2ac94b3a0b7501cbcaf
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-05-13 11:11:33 -0500

    Add files via upload

---
+++ scripts/Mud_Map_New_Colonies.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+library(ggrepel)
+
+# Seleccionar un archivo
+file <- file.choose()
+#Extract file name
+basename <- basename(file)
+#Remove characters that not include site or transect
+clean_basename <- basename |>
+  str_remove("Colonies_data_csv_") |>
+  str_remove(".csv")
+print(clean_basename)
+#Extract Site_name and Transect
+Site_name <- str_split_i(clean_basename, "_", 1)
+Transect <- str_split_i(clean_basename, "_", 2)
+
+#Load data
+Colonies_data <- read.csv(file)
+
+
+#Remove colonies that are from the other transect part. Use: |>  dplyr::filter(!Colony_ID %in% c())
+Colonies_data <- Colonies_data[!duplicated(Colonies_data$Colony_ID), ]
+#Tag data depending of Poles or Colonies Type (Poles are Capital letter and Colonies are numbers)
+Colonies_data$Type <- case_when(
+  grepl("^[A-Z]$", Colonies_data$Colony_ID) ~ "Poles",
+  grepl("25m", Colonies_data$Colony_ID) ~ "25m",
+  grepl("NC_1", Colonies_data$Colony_ID) ~ "NC_1",
+  grepl("NC_3", Colonies_data$Colony_ID) ~ "NC_3",
+  grepl("^[0-9]+$", Colonies_data$Colony_ID) |
+    grepl("^unk[0-9]+$", Colonies_data$Colony_ID) ~ "Colonies",
+  TRUE ~ "Other"
+)
+
+#Create subsets of data by Type (Colonies or letter)
+subset_words <- (Colonies_data[Colonies_data$Type == "Poles", ])
+subset_words <- subset_words[order(subset_words$Colony_ID), ]
+subset_numbers <- (Colonies_data[Colonies_data$Type == "Colonies", ])
+
+
+#Create subtitle depending on the transect label
+Subtitle_plot <- ifelse(Transect == "0to25", "0-25 m", "25-50 m")
+
+title_name <- paste("Mud Map of coral colonies in", Site_name)
+
+#Build the map
+ggplot(
+  Colonies_data,
+  aes(
+    x = Colonies_data$X_Real_World_Position,
+    y = Colonies_data$Y_Real_World_Position,
+  )
+) +
+  scale_color_manual(
+    values = c("black", "darkred", "#E5A94E", "darkorange", "darkblue")
+  ) +
+  scale_shape_manual(values = c(10, 18, 15, 15, 16)) +
+  geom_path(
+    data = subset_words,
+    aes(
+      x = subset_words$X_Real_World_Position,
+      y = subset_words$Y_Real_World_Position
+    ),
+    color = "darkblue"
+  ) +
+  geom_point(aes(color = Type, shape = Type), size = 7, alpha = 1) +
+  #Fixed x and y axis to get relative location.
+  coord_fixed() +
+  labs(title = title_name, subtitle = Subtitle_plot) +
+  xlab("X (meters)") +
+  ylab("Y (meters)") +
+  # Stablish X and Y limits
+  # Add +- 1 or 2 to include colonies near the axis
+  ylim(
+    min(Colonies_data$Y_Real_World_Position) - 1,
+    max(Colonies_data$Y_Real_World_Position) + 2
+  ) +
+  xlim(
+    min(Colonies_data$X_Real_World_Position) - 1,
+    max(Colonies_data$X_Real_World_Position) + 2
+  ) +
+  #Label Colonies and Poles IDs and modify vertical and horizontal justifation depending on the map
+  geom_text_repel(
+    aes(label = Colony_ID),
+    size = 5,
+    box.padding = 1.3,
+    segment.color = "black",
+    family = "Impact",
+    direction = "both",
+    max.overlaps = Inf
+  ) +
+  #Remove axes traces
+  theme_void() +
+  theme(
+    text = element_text("Impact", size = 15),
+    plot.title = element_text(hjust = 0.5, size = 23),
+    legend.position = "top",
+  )
+#Export in pdf with 1500 width - 500 heigh to be modified in InkScape or AdobeIllustrator
+#If having error saving plot, export using Rstudio or change font type and size from geom_text_repel and theme:text
